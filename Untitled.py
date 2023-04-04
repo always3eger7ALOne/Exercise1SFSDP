8cd8882321ccb31453387fbd583bba42b9be0cc8
Egor <sarukhanove1805@mail.ru>
2023-04-04 03:08:31 +0300

    lolo

--- Untitled.py
+++ Untitled.py
@@ -1,14 +1,6 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
 
     count = 0
 
